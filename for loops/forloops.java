1add020d73613673619c9997729770bbc0504d96
Anusjyothi <janu67058@gmail.com>
2025-12-06 11:33:31 +0530

    Add files via upload

---
+++ for loops/forloops.java
@@ -0,0 +1,15 @@
+public class forloops {
+
+    public static void main(String[] args)
+    {
+        for(int i=1; i<=10; i++)
+        {
+
+            System.out.println("JOHN");
+
+        }
+
+
+
+    }
+}
---
+++ for loops/forloops2.java
@@ -0,0 +1,18 @@
+public class forloops2 {
+    public static void main(String[]args)
+    {
+        for(int i=1; i<=10; i+=1)
+        {
+            System.out.println(i);
+        }
+        System.out.println("");
+
+        //Reverse
+
+        for(int i=5; i>=1;--i)
+        {
+            System.out.println(i);
+        }
+
+    }    
+}
